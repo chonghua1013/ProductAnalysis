e1dfd3cc38749857b2d6d140d5e0ae6429f55226
wuchonghua <2573790269@qq.com>
2025-07-31 16:45:38 +0800

    初始化

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type User struct {
-	ID        string    `json:"id" gorm:"primaryKey"`
+	ID        int       `json:"id" gorm:"primaryKey"`
 	Name      string    `json:"name" gorm:"not null"`
 	Email     string    `json:"email" gorm:"unique;not null"` // 确保有这行
 	Password  string    `json:"-" gorm:"not null"`
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"github.com/chonghua1013/ProductAnalysis/internal/config"
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,5 +14,25 @@ func NewPostgresDB(cfg config.DatabaseConfig) (*gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+	return db, nil
+}
+
+func NewMySQLDB(cfg config.DatabaseConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%t&loc=%s",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.Name,
+		cfg.Charset,
+		cfg.ParseTime,
+		cfg.Loc,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
 	return db, nil
 }
